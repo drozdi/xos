ddff4f885e1a4872e8e6e4bea35e78b590f74d99
Drozdi <drozdi-klim@yandex.ru>
2025-05-05 23:26:04 +0300

    mantine 8

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
+import '@mantine/core/styles/baseline.css';
+import '@mantine/core/styles/default-css-variables.css';
+import '@mantine/core/styles/global.css';
 import './App.css';
 import { Layout } from './components/layout';
 
 function App() {
 	return (
 		<Layout container>
+			<div slot="header">header</div>
+			<div slot="footer">footer</div>
 			<div slot="left">left</div>
+			<div slot="right">right</div>
 			<div>123</div>
 		</Layout>
 	);
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,22 @@
 import { MantineProvider } from '@mantine/core';
+import '@mantine/core/styles/ActionIcon.css';
 import '@mantine/core/styles/AppShell.css';
+import '@mantine/core/styles/CloseButton.css';
+import '@mantine/core/styles/Flex.css';
+import '@mantine/core/styles/FloatingIndicator.css';
 import '@mantine/core/styles/global.css';
+import '@mantine/core/styles/Group.css';
+import '@mantine/core/styles/InlineInput.css';
+import '@mantine/core/styles/Input.css';
+import '@mantine/core/styles/Loader.css';
+import '@mantine/core/styles/ModalBase.css';
+import '@mantine/core/styles/Overlay.css';
+import '@mantine/core/styles/Paper.css';
+import '@mantine/core/styles/Popover.css';
+import '@mantine/core/styles/ScrollArea.css';
 import '@mantine/core/styles/Skeleton.css';
+import '@mantine/core/styles/UnstyledButton.css';
+import '@mantine/core/styles/VisuallyHidden.css';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 
